291c8b7df21c9ff27a1955c8d848def6a74d63cc
Sandeep <sandeepkumardan@gmail.com>
2020-09-29 21:32:34 +0530

    Added minor changes

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminController');
 
 router.post('/adminauth', adminController.login);
-router.post('/test', adminController.test);
-router.post('/auth', adminController.authCheck);
+router.get('/test', adminController.test);
+router.get('/authcheck', adminController.authCheck);
 
 
 module.exports = router;
